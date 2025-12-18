510f7c10616a602c34df348d89c6088fdcff85a3
Binary-Human <mll.sara.bakri@gmail.com>
2025-12-18 15:53:42 +0100

    implement gridsearch to find best hyperparameters

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -2,11 +2,14 @@ import time
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split, cross_val_predict, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_predict, cross_val_score, GridSearchCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, classification_report, ConfusionMatrixDisplay
 
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.utils import resample
+
+import joblib
 
 class DecisionTreePipeline:
     def __init__(self, model, model_type = RandomForestClassifier, test_size=0.3, random_state=42):
@@ -87,7 +90,7 @@ class DecisionTreePipeline:
         plt.show()
 
     def cross_validation(self, cv=5):
-
+        
         labels_pred = cross_val_predict(self.model, self.features_test, self.labels_test, cv=cv)
 
         accuracy = cross_val_score(self.model, self.features_test, self.labels_test, cv=cv, scoring="accuracy")
@@ -111,6 +114,47 @@ class DecisionTreePipeline:
         disp.plot()
         plt.show()
 
+    def grid_search_impl(self, param_grid):
+        
+        MAX_SAMPLES = 166316
+
+        grid_search = GridSearchCV(
+            estimator=self.model,
+            param_grid=param_grid,
+            scoring="accuracy",
+            cv=5,
+            n_jobs=-1
+        )
+
+        feature_train_resampled, labels_train_resampled = resample(
+            self.features_train,
+            self.labels_train,
+            n_samples=MAX_SAMPLES,
+            random_state=42,
+            stratify=self.labels_train
+        )
+        
+        start = time.time()
+        grid_search.fit(feature_train_resampled, labels_train_resampled)
+        exec_time = time.time() - start
+
+        best_params = grid_search.best_params_
+
+        print("Best hyperparameters:", grid_search.best_params_)
+        print("Execution time:", exec_time)
+        print("Best CV accuracy:", grid_search.best_score_)
+
+        self.model = self.model_type( random_state=None, **best_params)
+
+        self.train()
+
+        print("\n--- Train set evaluation ---\n")
+        pipeline.evaluate(self.features_train, self.labels_train)
+        print("\n--- Test set evaluation ---\n")
+        pipeline.evaluate(self.features_test, self.labels_test)
+
+        joblib.dump(grid_search.best_estimator_,'BestModel_XXXX.joblib')
+
 pipeline = DecisionTreePipeline(model=RandomForestClassifier())
 
 pipeline.load_data(
@@ -122,10 +166,35 @@ pipeline.split_data()
 pipeline.scale_column("AGEP", scaler_type="standard")
 pipeline.scale_column("WKHP", scaler_type="minmax")
 
+""" 
 pipeline.train()
 
 print("\n--- Train set evaluation ---\n")
 pipeline.evaluate(pipeline.features_train, pipeline.labels_train)
 print("\n--- Test set evaluation ---\n")
 pipeline.evaluate(pipeline.features_test, pipeline.labels_test)
-pipeline.cross_validation()
+pipeline.cross_validation()  """
+
+
+param_grid_rf = {
+    "n_estimators": [50, 100, 200],
+    "max_depth": [None, 5, 10, 20],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
+
+param_grid_adaB =  {
+    "n_estimators": [50, 100, 200],
+    "learning_rate": [0.01, 0.1, 1],
+    "algorithm": ["SAMME", "SAMME.R"]
+}
+
+param_grid_gBoost = {
+    "n_estimators": [50, 100, 200],
+    "max_depth": [3, 5, 7],
+    "learning_rate": [0.01, 0.1, 0.2, 1],
+    "subsample": [0.5, 0.7, 1.0]
+    
+}
+
+pipeline.grid_search_impl(param_grid_rf)
